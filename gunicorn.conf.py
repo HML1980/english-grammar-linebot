312afb8c54c1905a9f4c498ee317ff79f44807ae
HML1980 <woras.tw@gmail.com>
2025-09-11 07:13:37 +0000

    升級到 PostgreSQL 支援100人同時使用

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,23 +1,19 @@
 import os
 
-# 單一 worker 配置
 bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
-workers = 1
+workers = 4
 worker_class = "sync"
-worker_connections = 1
+worker_connections = 1000
 threads = 1
 
-# 超時設定
 timeout = 120
 keepalive = 2
 graceful_timeout = 30
 
-# 預載和記憶體設定
 preload_app = True
 max_requests = 1000
 max_requests_jitter = 50
 
-# 日誌
 accesslog = "-"
 errorlog = "-"
 loglevel = "info"
